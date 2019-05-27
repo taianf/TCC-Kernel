0c3325e13061abbf1160dd1df1fde21e123a5180
Taian Fonseca Feitosa <taianmeca@gmail.com>
2019-05-27 20:14:16 -0300

    scripts

---
+++ kernels/install-kernels.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+tar xzf rpi-kernel.tgz
+tar xzf prt-kernel.tgz
+tar xzf xen-kernel.tgz
+cd boot
+sudo cp -rd * /boot/
+cd ../lib
+sudo cp -dr * /lib/
+cd ../overlays
+sudo cp -d * /boot/overlays
+cd ..
+sudo cp -d bcm* /boot/
