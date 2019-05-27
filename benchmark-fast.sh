19261417590f16f738f46a83892db6a27747e981
Taian Fonseca Feitosa <taianmeca@gmail.com>
2019-05-27 20:42:13 -0300

    up

--- benchmark-fast.sh
+++ benchmark-fast.sh
@@ -24,8 +24,8 @@ echo usleep_range > delay_type
 echo 100 > progress_interval
 echo 1000 > reps
 
-mkdir -p "results"
-mkdir -p "results/proc"
+# mkdir -p "results"
+mkdir -p ~/results/proc
 
 echo > prepare_trigger
 
@@ -34,24 +34,24 @@ echo > prepare_trigger
 set +e
 
 # Gather system information before the benchmark.
-cp -vf /proc/schedstat "results/proc/schedstat.before"
-cp -vf /proc/softirqs "results/proc/softirqs.before"
-cp -vf /proc/stat "results/proc/stat.before"
-cp -vf /proc/interrupts "results/proc/interrupts.before"
+cp -vf /proc/schedstat ~/results/proc/schedstat.before
+cp -vf /proc/softirqs ~/results/proc/softirqs.before
+cp -vf /proc/stat ~/results/proc/stat.before
+cp -vf /proc/interrupts ~/results/proc/interrupts.before
 
 # Execute the benchmark
 echo > do_trigger
 
 # Gather system information after the benchmark.
-cp -vf /proc/interrupts "results/proc/interrupts.after"
-cp -vf /proc/stat "results/proc/stat.after"
-cp -vf /proc/softirqs "results/proc/softirqs.after"
-cp -vf /proc/schedstat "results/proc/schedstat.after"
+cp -vf /proc/interrupts ~/results/proc/interrupts.after
+cp -vf /proc/stat ~/results/proc/stat.after
+cp -vf /proc/softirqs ~/results/proc/softirqs.after
+cp -vf /proc/schedstat ~/results/proc/schedstat.after
 
 # Save the benchmark results.
 echo > postprocess_trigger
-cp -vrf . "results/intsight"
+cp -vrf . ~/results/intsight
 
 # Gather general system information.
-cp -vf /proc/version "results/proc/version"
-cp -vf /proc/config.gz "results/proc/config.gz"
+cp -vf /proc/version ~/results/proc/version
+cp -vf /proc/config.gz ~/results/proc/config.gz
