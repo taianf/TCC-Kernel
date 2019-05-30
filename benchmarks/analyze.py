63e34573929dff96ab683f14e3a488da0231b487
Taian Fonseca Feitosa <taianmeca@gmail.com>
2019-05-29 22:22:36 -0300

    python analyze

--- benchmarks/analyze.py
+++ benchmarks/analyze.py
@@ -3,14 +3,15 @@ import csv
 import sys
 
 print("This is the name of the script: \"" + sys.argv[0] + "\"")
-if len(sys.argv) != 2:
-    print("Especify root directory of benchmark!")
+if len(sys.argv) != 3:
+    print("Especify root directory of benchmark and timestamp type!")
+    print("Example: results-fast pmccntr")
     exit(1)
 
 root_csv_folder = sys.argv[1] + "/intsight/csv_results/"
 
 measures_file = root_csv_folder + "name"
-timestamps_file = root_csv_folder + "pmccntr"
+timestamps_file = root_csv_folder + sys.argv[2]
 response_times = []
 total_time = 0
 
