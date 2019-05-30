c4956e90410bc2e2265e419dc79791fcd832fda5
Taian Fonseca Feitosa <taianmeca@gmail.com>
2019-05-29 21:33:12 -0300

    python analyze

---
+++ benchmarks/analyze.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import csv
+import sys
+
+print("This is the name of the script: \"" + sys.argv[0] + "\"")
+
+root_csv_folder = "results-idle/rpi/fast/intsight/csv_results/"
+
+measures_file = root_csv_folder + "name"
+timestamps_file = root_csv_folder + "pmccntr"
+response_times = []
+total_time = 0
+
+with open(measures_file, 'r') as measures_csv, open(
+        timestamps_file, 'r') as timestamps_csv:
+    measures_reader = csv.reader(measures_csv, delimiter=',')
+    timestamps_reader = csv.reader(timestamps_csv, delimiter=',')
+    measures = list(measures_reader)
+    timestamps = list(timestamps_reader)
+
+    results = list(map(lambda x, y: dict(zip(x, y)), measures, timestamps))
+
+for result in results:
+    if ("irq" in result.keys()) & ("softirq" in result.keys()):
+        if (result["irq"] != "0") & (result["softirq"] != "0"):
+            response_times.append(int(result["softirq"]) - int(result["irq"]))
+            print("line: " + str(len(response_times)) + " irq: " + result["irq"] + " softirq: " + result[
+                "softirq"] + " total time: " + str(response_times[-1]))
+
+print("lines: " + str(len(response_times)))
+if len(response_times) != 0:
+    print("Average time: " + str(sum(response_times) / len(response_times)))
+    print("Minimum time: " + str(min(response_times)))
+    print("Maximum time: " + str(max(response_times)))
