8837adafacb7ee4723cca25881a6c9a74845b71e
Taian Fonseca Feitosa <taianmeca@gmail.com>
2019-11-09 11:30:59 -0300

    update scripts

--- benchmarks/tidy-chunk-result.py
+++ benchmarks/tidy-chunk-result.py
@@ -17,7 +17,7 @@ with open(sys.argv[1] + ".csv", mode="w") as final:
     final.write("position,run,name,ktime_mono_fast\n")
     for x in range(name_df.size - 1):
         for y in range(50):
-            if str(name_df.iloc[x][y]) != "nan":
+            if str(name_df.iloc[x][y]) != "nan" and str(time_df.iloc[x][y]) != "nan":
                 final.write(
                     str(y + 1) + "," + str(x + 1) + "," + str(name_df.iloc[x][y]) + "," + str(
                         int(time_df.iloc[x][y])) + "\n")
